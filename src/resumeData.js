f620b998240827b166802c4077ddab34634f5117
Daksh Singh <dakshsingh2608@gmail.com>
2021-12-28 01:14:38 +0530

    intialcommit

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Daksh Singh",
+    "role": "MERN Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/daksh-singh-2959071aa/",
+    "EmailID":"dakshsingh2608@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and Beckend Development and like to learn about new technologies, write some blogs or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/daksh-singh-2959071aa/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Daksh-964",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a 3rd year student at Dr.D.Y Patil Institute of Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"Dr.D.Y Patil Institute of Technology",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2023",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"MERN Stack Development, C++,JAVA, Python,SQL",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"C"
+      },
+      {
+        "skillname":"JAVA"
+      },
+      {
+        "skillname":"ProblemSolving"
+      },{
+        "skillname":"SQL"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Tinder Clone",
+        "description":"Web App",
+        "imgurl":"images/portfolio/tinder.png"
+      },
+      {
+        "name":"Amazon Clone",
+        "description":"web app",
+        "imgurl":"images/portfolio/Amazon.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
